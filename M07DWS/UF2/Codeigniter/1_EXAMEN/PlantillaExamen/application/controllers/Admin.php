d68f3dbce3f7efc0febbc7b5b3c0deac79be5e55
Jordi Valles <w2.jvalles@infomila.info>
2019-02-01 04:42:28 +0100

    Update Ci pre examen

---
+++ M07DWS/UF2/Codeigniter/1_EXAMEN/PlantillaExamen/application/controllers/Admin.php
@@ -0,0 +1,87 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+// session_start();
+class Admin extends CI_Controller {
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();		
+	}
+
+	public function index()
+	{    
+		//cursos disponibles
+		$data['cursos'] = $this->ModelConsultes->getCursos();
+		
+		//usuaris (propietaris)
+		$data['propietaris'] = $this->ModelConsultes->getPropietaris();
+		
+		$this->load->view('Admin',$data);
+	}
+	
+	public function crearCurs(){
+		
+		//guardem la data
+		$data['id'] = $this->input->post('fid');
+		$data['propietari'] = $this->input->post('fpropietari');
+        
+		$data['check'] = $this->ModelConsultes->checkCurs($data);
+		
+		if($data['check']==false){
+            
+			$this->ModelConsultes->crearCurs($data);
+            
+            //agafem el mail del propietari assignat
+            $data['mail'] = $this->ModelConsultes->getMailPropietari($data);
+            //echo $data['mail'][0]["mail"];
+            //var_dump($data['mail']);
+            
+            //enviar el correu
+            $config = Array(
+                'protocol' => 'smtp',
+                'smtp_host' => 'ssl://smtp.googlemail.com',
+                'smtp_port' => 465,
+                'smtp_user' => 'w2.jvalles@infomila.info',
+                'smtp_pass' => '47124284K',
+                'mailtype'  => 'html', 
+                'charset'   => 'iso-8859-1'
+            );
+            
+            $this->load->library('email', $config);
+            $this->email->set_newline("\r\n");
+            
+            $this->email->from('w2.jvalles@infomila.info', 'Administrador');
+            //$this->email->to('w2.rperez@infomila.info');
+            $this->email->to($data['mail'][0]["mail"]);
+            
+            $this->email->subject('Assignacio de curs');
+            $this->email->message("Hola,t'informem que tens un nou curs assignat. Ja pots iniciar sessio al teu compte per a gestionar-lo.");  
+            
+            $result = $this->email->send();
+            
+            if($result){
+                $data['exit'] = "Curs creat correctament. Mail enviat correctament.";
+            }else{
+                $data['error'] = "Curs creat correctament però hi ha hagut un error al enviar el mail.";
+            }
+            
+		}else{
+			$data['error'] = "Aquest curs ja existeix!";
+		}
+        
+		//cursos disponibles
+		$data['cursos'] = $this->ModelConsultes->getCursos();
+		
+		//usuaris (propietaris)
+		$data['propietaris'] = $this->ModelConsultes->getPropietaris();
+		
+		$this->load->view('Admin',$data);
+        
+	}
+	
+}
+
+
+
+
---
+++ M07DWS/UF2/Codeigniter/1_EXAMEN/PlantillaExamen/application/controllers/Propietari.php
@@ -0,0 +1,155 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+// session_start();
+class Propietari extends CI_Controller {
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->database();	
+	}
+
+	public function index()
+	{    
+		$data['id'] = $this->session->codiPropietari;
+		
+		//cursos del propietari
+		$data['cursos'] = $this->ModelConsultes->getCursosPropietari($data);
+		
+		$this->load->view('Propietari',$data);
+	}
+	
+	public function modificarCurs(){
+		
+		//id del curs
+		$data['idcurs'] = $this->uri->segment('3');
+		
+        $data['dades'] = $this->ModelConsultes->getDadesCurs($data);
+        
+        //var_dump($data['dades']);
+        
+        $this->load->view('ModificarCurs',$data);
+        
+		//redirect('Menu/index', 'refresh');
+	}
+	
+    
+    public function updateCurs(){
+        
+        //guardem id
+        $data['cid'] = $this->input->post('fid');
+        
+		//guardem el titol
+		$data['titol'] = $this->input->post('ftitol');
+        
+		//guardem data inici
+		$data['datainici'] = $this->input->post('fdatainici');
+		
+		//guardem hores totals
+		$data['htotals'] = $this->input->post('fhtotals');
+		
+		//guardem hores setmanals
+		$data['hsetmanals'] = $this->input->post('fhsetmanals');
+		
+		//modifiquem el curs
+		$this->ModelConsultes->actualitzarCurs($data);
+		
+		$data['exit'] = "Els canvis s'han efectuat correctament.";
+		
+		//un cop modificat tornem al menú del propietari
+		$data['id'] = $this->session->codiPropietari;
+		
+        //cursos del propietari
+		$data['cursos'] = $this->ModelConsultes->getCursosPropietari($data);
+		
+		$this->load->view('Propietari',$data);
+	}
+    
+	public function generarCalendari(){
+		
+		//id del curs
+		$data['idcurs'] = $this->uri->segment('3');
+		
+		/*
+		$prefs = array(
+			'local_time'   => 'time()',
+			'start_day'    => 'monday',
+			'month_type'   => 'long',
+			'day_type'     => 'short',
+			'show_next_prev' => true,
+			'next_prev_url'   => '',
+			'show_other_days' => false
+		);
+
+		$this->load->library('calendar', $prefs);
+
+		$data = array(
+			3  => 'http://example.com/news/article/2006/06/03/',
+			7  => 'http://example.com/news/article/2006/06/07/',
+			13 => 'http://example.com/news/article/2006/06/13/',
+			26 => 'http://example.com/news/article/2006/06/26/'
+		);
+
+		echo $this->calendar->generate(2018, 12, $data);
+		*/
+		
+		$prefs['template'] = '
+
+			{table_open}<table border="0" cellpadding="0" cellspacing="0">{/table_open}
+
+			{heading_row_start}<tr>{/heading_row_start}
+
+			{heading_previous_cell}<th><a href="{previous_url}">&lt;&lt;</a></th>{/heading_previous_cell}
+			{heading_title_cell}<th colspan="{colspan}">{heading}</th>{/heading_title_cell}
+			{heading_next_cell}<th><a href="{next_url}">&gt;&gt;</a></th>{/heading_next_cell}
+
+			{heading_row_end}</tr>{/heading_row_end}
+
+			{week_row_start}<tr>{/week_row_start}
+			{week_day_cell}<td>{week_day}</td>{/week_day_cell}
+			{week_row_end}</tr>{/week_row_end}
+
+			{cal_row_start}<tr>{/cal_row_start}
+			{cal_cell_start}<td>{/cal_cell_start}
+			{cal_cell_start_today}<td>{/cal_cell_start_today}
+			{cal_cell_start_other}<td class="other-month">{/cal_cell_start_other}
+
+			{cal_cell_content}<a href="{content}">{day}</a>{/cal_cell_content}
+			{cal_cell_content_today}<div class="highlight"><a href="{content}">{day}</a></div>{/cal_cell_content_today}
+
+			{cal_cell_no_content}{day}{/cal_cell_no_content}
+			{cal_cell_no_content_today}<div class="highlight">{day}</div>{/cal_cell_no_content_today}
+
+			{cal_cell_blank}&nbsp;{/cal_cell_blank}
+
+			{cal_cell_other}{day}{/cal_cel_other}
+
+			{cal_cell_end}</td>{/cal_cell_end}
+			{cal_cell_end_today}</td>{/cal_cell_end_today}
+			{cal_cell_end_other}</td>{/cal_cell_end_other}
+			{cal_row_end}</tr>{/cal_row_end}
+
+			{table_close}</table>{/table_close}
+		';
+
+		$this->load->library('calendar', $prefs);
+
+		echo $this->calendar->generate();
+	}
+	
+	public function eliminarReserva(){
+		//id
+		$data['id'] = $this->uri->segment('3');
+		$data['data'] = $this->uri->segment('4');
+		//echo $data['id']." ".$data['data'];
+		
+		$this->ModelConsultes->deleteReserva($data);
+		$this->ModelConsultes->habilitarData($data);
+		redirect('Menu/index', 'refresh');
+	}
+	
+}
+
+
+
+
